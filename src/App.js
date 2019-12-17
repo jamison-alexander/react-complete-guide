eaad95ae3430f08364368d7beb67166c6f471607
TURNER, Bryce Alexander <bturner46@gatech.edu>
2019-12-16 20:38:05 -0700

    Working through beginning of hooks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  render() {
+const app = props => {
+    const [personsState, setPersonsState ] = useState( {
+    persons: [
+      {
+        name: 'Nylaa',
+        age: 28
+      }, 
+      {
+        name: 'Brytce',
+        age: 66
+      }
+    ],
+  });
+
+  const[otherState, setOtherState] = useState('really great state');
+  console.log(personsState, otherState);
+    
+  const switchNameHandler = () => {
+    setPersonsState({persons: [
+      {
+        name: 'Nifoolar',
+        age: 28
+      }, 
+      {
+        name: 'Brytce',
+        age: 66
+      }
+    ]});
+  };
+
+  
+
+
     return (
     <div className="App">
       <h1>A React App!</h1>
-      <Person name="Nylaa" age="28" />
-      <Person name="Brytce" age="66" />
+      <button onClick={switchNameHandler}>Switch</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>My hobbies: Pashmi, Instagram</Person>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}></Person>
     </div>
   );
-}
-}
-
-export default App;
+    }
+export default app;
